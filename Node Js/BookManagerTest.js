d6f46741816b3379a5f01675f30bafacf4284712
Creksz <155244523+Creksz@users.noreply.github.com>
2025-09-04 14:26:47 +0700

    Book Manager by NodeJS
    NodeJS version

---
+++ Node Js/BookManagerTest.js
@@ -0,0 +1,54 @@
+const Book = require('../book');
+const BookManager = require('../bookManager');
+
+describe('BookManager', () => {
+  let bookManager;
+
+  beforeEach(() => {
+    bookManager = new BookManager();
+  });
+
+  test('Test menambahkan buku', () => {
+    const book = new Book("Test Book", "Test Author", 2023);
+    bookManager.addBook(book);
+    expect(bookManager.getBookCount()).toBe(1);
+  });
+
+  test('Test menghapus buku yang ada', () => {
+    const book = new Book("To Remove", "Author", 2023);
+    bookManager.addBook(book);
+
+    const removed = bookManager.removeBook("To Remove");
+    expect(removed).toBe(true);
+    expect(bookManager.getBookCount()).toBe(0);
+  });
+
+  test('Test menghapus buku yang tidak ada', () => {
+    const removed = bookManager.removeBook("Buku Tidak Ada");
+    expect(removed).toBe(false);
+  });
+
+  test('Test mencari buku berdasarkan author', () => {
+    const book1 = new Book("Book 1", "Author A", 2020);
+    const book2 = new Book("Book 2", "Author B", 2021);
+    const book3 = new Book("Book 3", "Author A", 2022);
+    bookManager.addBook(book1);
+    bookManager.addBook(book2);
+    bookManager.addBook(book3);
+
+    const booksByAuthorA = bookManager.findBooksByAuthor("Author A");
+    expect(booksByAuthorA.length).toBe(2);
+    expect(booksByAuthorA).toEqual(expect.arrayContaining([book1, book3]));
+  });
+
+  test('Test mendapatkan semua buku', () => {
+    const book1 = new Book("Book 1", "Author A", 2020);
+    const book2 = new Book("Book 2", "Author B", 2021);
+    bookManager.addBook(book1);
+    bookManager.addBook(book2);
+
+    const allBooks = bookManager.getAllBooks();
+    expect(allBooks.length).toBe(2);
+    expect(allBooks).toEqual(expect.arrayContaining([book1, book2]));
+  });
+});
